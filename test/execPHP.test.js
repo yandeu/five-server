f55aca35b020c8428171812271e52e782691c833
yandeu <20306025+yandeu@users.noreply.github.com>
2021-11-13 22:30:54 +0100

    Test execPHP

---
+++ test/execPHP.test.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const os = require('os')
+const { ExecPHP } = require('../lib/utils/execPHP')
+
+let phpPath = os.platform() === 'win32' ? 'C:\\xampp\\php\\php.exe' : '/usr/bin/php'
+
+const PHP = new ExecPHP()
+PHP.php = phpPath
+
+const mock_res = {
+  status: _status => {}
+}
+
+describe('parse from file', function () {
+  it('should work', async () => {
+    const html = await PHP.parseFile(path.join(__dirname, 'data/bonjour.php'), mock_res)
+    expect(html).toContain(`<p>Bonjour le monde!</p>`)
+  })
+})
