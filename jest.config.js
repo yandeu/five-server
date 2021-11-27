92915f37015feb4815fd826f2ad31005d0fbb4e5
yandeu <20306025+yandeu@users.noreply.github.com>
2021-11-28 00:23:05 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['lib/*.js', 'lib/utils/*.js', 'lib/middleware/*.js'],
+  coverageReporters: ['html', 'lcov', 'text'],
   maxConcurrency: 1,
   maxWorkers: 1,
   testTimeout: 30_000
