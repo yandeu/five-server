cac96e8556126fae7a535cc8fa676a44e5f8de18
yandeu <20306025+yandeu@users.noreply.github.com>
2024-03-17 14:58:41 +0100

    Test php relative import path

---
+++ test/data/sub/content.php
@@ -0,0 +1,5 @@
+<?php
+include "../header.php";
+?>
+
+<p>I am the content</p>
--- test/execPHP.test.js
+++ test/execPHP.test.js
@@ -8,12 +8,17 @@ const PHP = new ExecPHP()
 PHP.php = phpPath
 
 const mock_res = {
-  status: _status => { }
+  status: _status => {}
 }
 
 describe('parse from file', function () {
   it('should work', async () => {
     const html = await PHP.parseFile(path.join(__dirname, 'data/bonjour.php'), mock_res)
     expect(html).toContain(`<p>Bonjour le monde!</p>`)
+  })
+
+  it('should work with relative import paths', async () => {
+    const html = await PHP.parseFile(path.join(__dirname, 'data/sub/content.php'), mock_res)
+    expect(html.replace(/\r?\n/g, '')).toContain('<p>I am the header</p><p>I am the content</p>')
   })
 })
